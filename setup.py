bacf5797a0b229fc2bea4dc2f89f81b032ab873c
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2015-11-01 19:57:07 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='Python-Modbus',
+      version='0.1.0',
+      author='Auke Willem Oosterhoff',
+      author_email='oosterhoff@baopt.nl',
+      description='Implementation of the Modbus protocol in pure Python.',
+      url='https://github.com/AdvancedClimateSystems/python-modbus/',
+      packages=[
+          'modbus',
+      ],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Embedded Systems',
+      ])
