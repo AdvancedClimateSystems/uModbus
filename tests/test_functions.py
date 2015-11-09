01154e388c3ce4f8d162ef454a8fc7eaffa6ec58
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2015-11-09 14:47:32 +0100

    #5 Improve tests for 'function_factory()'.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -8,7 +8,8 @@ except ImportError:
     from mock import MagicMock
 
 from umodbus.route import Map
-from umodbus.exceptions import IllegalDataValueError, IllegalDataAddressError
+from umodbus.exceptions import (IllegalFunctionError, IllegalDataValueError,
+                                IllegalDataAddressError)
 from umodbus.functions import (function_factory, ReadCoils,
                                WriteMultipleValueFunction, ReadDiscreteInputs,
                                ReadInputRegisters, ReadHoldingRegisters,
@@ -103,30 +104,12 @@ def test_function_factory(pdu, cls):
     assert isinstance(function_factory(pdu), cls)
 
 
-def test_caching_of_function_factory():
-    """ Equal calls to :meth:`function_factory` should return same response.
+def test_function_factory_raising_illegal_function_error():
+    """ Calling function with PDU containing invalid function code should result
+    in an IllegalFunctionError.
     """
-    function_code = 1
-    starting_address = 100
-    quantity = 3
-
-    pdu = struct.pack('>BHH', function_code, starting_address, quantity)
-
-    # Call method twice, both with same input...
-    function_1 = function_factory(pdu)
-    function_2 = function_factory(pdu)
-
-    # ...output should be the same.
-    assert id(function_1) == id(function_2)
-
-    starting_address = 101
-    pdu = struct.pack('>BHH', function_code, starting_address, quantity)
-
-    # But when called with different pdu...
-    function_3 = function_factory(pdu)
-
-    # ...output should not be the same as previous calls.
-    assert id(function_1) is not id(function_3)
+    with pytest.raises(IllegalFunctionError):
+        function_factory(b'\x00')
 
 
 class TestReadFunction:
