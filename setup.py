55ea71ef8f8729ca5fec895fff1f392434e9c4af
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2015-11-11 09:51:06 +0100

    Add long description and license to setup.py.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+"""
+uModbus is a pure Python implementation of the Modbus protocol with support
+for Python 2.7, 3.3, 3.4, 3.5 and Pypy. uModbus has no runtime depedencies.
+
+"""
 from setuptools import setup
 
 setup(name='uModbus',
@@ -7,6 +12,8 @@ setup(name='uModbus',
       author_email='oosterhoff@baopt.nl',
       description='Implementation of the Modbus protocol in pure Python.',
       url='https://github.com/AdvancedClimateSystems/umodbus/',
+      long_description=__doc__,
+      license='MPL',
       packages=[
           'umodbus',
       ],
