9cf56c9b811c40fc0a2b879574a1d9eb5ec0d5e4
Greg Lutostanski <gregory.lutostanski@canonical.com>
2018-05-18 19:07:11 -0500

    Fix test_message_with_timeout
    Need to send a real request (so we know how much data to wait for), use
    a request that has more response data than the request (in terms of
    bytes) to trigger when a Timeout should be detected properly.

--- tests/unit/client/serial/test_rtu.py
+++ tests/unit/client/serial/test_rtu.py
@@ -1,12 +1,17 @@
 import pytest
 from serial import serial_for_url
 
-from umodbus.client.serial.rtu import send_message
+from umodbus.client.serial.rtu import send_message, read_coils
 
 
 def test_send_message_with_timeout():
-    """ Test if TimoutError is raised when serial port doesn't receive data."""
+    """ Test if TimoutError is raised when serial port doesn't receive enough
+    data.
+    """
     s = serial_for_url('loop://', timeout=0)
+    # as we are using a loop, we need the request will be read back as response
+    # to test timeout we need a request with a response that needs more bytes
+    message = read_coils(slave_id=0, starting_address=1, quantity=40)
 
     with pytest.raises(ValueError):
-        send_message(b'', s)
+        send_message(message, s)
