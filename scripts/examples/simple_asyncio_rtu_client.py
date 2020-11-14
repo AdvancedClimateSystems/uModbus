8ebe20900e7b832b683cb21e68c9e1982adb2c47
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-11-14 16:22:13 +0100

    Add simple asyncio RTU serial client exmaple

---
+++ scripts/examples/simple_asyncio_rtu_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# scripts/example/simple_async_rtu_client.py
+import asyncio
+
+from serial_asyncio import open_serial_connection
+
+from umodbus.client.serial import rtu
+
+
+async def main():
+    reader, writer = await open_serial_connection(url='/dev/ttyS1', timeout=1)
+
+    # Returns a message or Application Data Unit (ADU) specific for doing
+    # Modbus TCP/IP.
+    message = tcp.write_multiple_coils(slave_id=1, starting_address=1, values=[1, 0, 1, 1])
+
+    # Response depends on Modbus function code. This particular returns the
+    # amount of coils written, in this case it is.
+    response = await tcp.async_send_message(message, reader, writer)
+
+    writer.close()
+    await writer.wait_closed()
+
+
+asyncio.run(main())
